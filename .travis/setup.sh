ff6c17a188fdd8199c23fdf32567ebe154a93bf4
SampannaKahu <sampyash@gmail.com>
2020-02-17 17:27:25 -0500

    Fixing the sregistry version in setup.sh. ALso uncommented the sudo line.

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -ex
 
-#sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
+sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
 
 # Check Python
 
 echo "Python Version:"
 python --version
-pip install --user sregistry[all]
+pip install sregistry
 sregistry version
 
 echo "sregistry Version:"
