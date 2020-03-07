2035c49bfdcc31cc03d83b89323a563952b4214a
SampannaKahu <sampyash@gmail.com>
2020-03-06 20:22:30 -0500

    Miscellaneous changes: 1. Added a new set of files_*.json which contain the randomized filenames. 2. Created a dedicated file for the k_80 job. 3. Modifying the ir_job to fix the clean of the previous model and now deleting the old files instead of zipping them since that was taking a lot of time.

---
+++ hpc/jobs/generate_file_batches.py
@@ -0,0 +1,12 @@
+import random
+import json
+
+if __name__ == "__main__":
+    with open("crude_list.txt") as f:
+        files = ["s3://" + file.strip().replace("_", "/", 2) for file in f if file and file.strip()]
+    random.shuffle(files, random=lambda: random.uniform(0, 1))
+    CHUNK_SIZE = 100
+    final = [files[i * CHUNK_SIZE:(i + 1) * CHUNK_SIZE] for i in range((len(files) + CHUNK_SIZE - 1) // CHUNK_SIZE)]
+    for idx, final_list in enumerate(final):
+        with open("../files_random/files_{}.json".format(idx), mode="w") as file_json:
+            json.dump(final_list, file_json, indent=4)
