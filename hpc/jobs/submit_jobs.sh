1de2064a13451a8b7e7c06172b7fedb9b9e85c85
SampannaKahu <sampyash@gmail.com>
2020-05-14 14:29:46 -0400

    Added a new command for submitting the jobs that timed out in the 0-23 range.

--- hpc/jobs/submit_jobs.sh
+++ hpc/jobs/submit_jobs.sh
@@ -6,4 +6,8 @@ sudo ./ir_job.sh 2>&1 | tee -a output.log
 
 ## For pregenerating training data
 
+# Default command
 sbatch --array=0-39 pregenerate_data.sh
+
+# For jobs that timed out
+sbatch --array=0,4,5,6,8,12,14,16,17,20 pregenerate_data.sh
