14a722f7656065e4db4e8a34f38d45e238dfb73d
SampannaKahu <sampyash@gmail.com>
2020-05-15 11:48:29 -0400

    Added the level 2 failures from the first batch

--- hpc/jobs/submit_jobs.sh
+++ hpc/jobs/submit_jobs.sh
@@ -14,3 +14,6 @@ sbatch --array=0,4,5,6,8,12,14,16,17,20 pregenerate_data.sh
 
 # For jobs that timed out (from 24 to 39)
 sbatch --array=24,25,28,31,32,33,34,35,37,38,39 pregenerate_data.sh
+
+# For jobs that faced the stale file handle error
+sbatch --array=8,16,20 pregenerate_data.sh
