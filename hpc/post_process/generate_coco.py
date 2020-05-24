4e5e9419794a1b698c85e6d9b88bd93f474fc5dc
SampannaKahu <sampyash@gmail.com>
2020-05-24 12:35:40 -0400

    Optimized the generate_coco script by using parallelism for a part of it.

--- hpc/post_process/generate_coco.py
+++ hpc/post_process/generate_coco.py
@@ -5,7 +5,9 @@ import glob
 import logging
 import torch
 import json
+import typing
 from PIL import Image
+from multiprocessing import Pool
 from coco_to_figure_boundaries import coco_to_fig_boundaries
 from figure_boundaries_train_test_split import split_train_test
 
@@ -87,6 +89,27 @@ def build_image(image_path: str, image_id: int, height: int, width: int):
     }
 
 
+def unzip_zip_file(zip_file_path: str, extract_dir: str = tmp_extract_dir) -> typing.Tuple[
+    typing.List[str], typing.List[str]]:
+    """
+    Takes in a zip file path. Unzips it in a temporary directory. And returns the list of the files unzipped.
+    Works only for flat file-structured zips.
+    :param zip_file_path: path os the zip file.
+    :param extract_dir: The directory to extract the data into.
+    :return: the list of path of the contents of the zip (all, png and pt)
+    """
+    process_unzip_dir = os.path.join(extract_dir, str(os.getpid()))
+    os.makedirs(process_unzip_dir, exist_ok=True)
+    zip = zipfile.ZipFile(zip_file_path)
+    zip.extractall(path=process_unzip_dir)
+    zip.close()
+    file_list = os.listdir(os.path.join(process_unzip_dir, 'tmp'))
+    png_paths = [os.path.join(process_unzip_dir, 'tmp', path) for path in file_list if '.png' in path]
+    pt_paths = [os.path.join(process_unzip_dir, 'tmp', path) for path in file_list if '.pt' in path]
+    assert len(png_paths) == len(pt_paths)
+    return png_paths, pt_paths
+
+
 os.makedirs(dataset_dir, exist_ok=True)
 os.makedirs(image_save_dir, exist_ok=True)
 shutil.rmtree(tmp_extract_dir, ignore_errors=True)
@@ -100,18 +123,17 @@ else:
 current_image_id = max([image['id'] for image in dataset['images']], default=0) + 1
 current_annotation_id = max([annotation['id'] for annotation in dataset['annotations']], default=0) + 1
 
-for path in glob.glob(os.path.join(job_output_directory, '*/*.zip'), recursive=True):
-    logger.info("Starting zipfile: {}".format(path))
-    zip = zipfile.ZipFile(path)  # TODO: Add exception handling for when the zipfile is malformed.
-    zip.extractall(path=tmp_extract_dir)
-    zip.close()
-    file_list = os.listdir(os.path.join(tmp_extract_dir, 'tmp'))
-    png_paths = [os.path.join(tmp_extract_dir, 'tmp', path) for path in file_list if '.png' in path]
-    pt_paths = [os.path.join(tmp_extract_dir, 'tmp', path) for path in file_list if '.pt' in path]
-    assert len(png_paths) == len(pt_paths)
-
-    png_paths.sort()
-    pt_paths.sort()
+zip_paths = glob.glob(os.path.join(job_output_directory, '*/*.zip'), recursive=True)
+batch_size = 7
+batches = [zip_paths[i:i + batch_size] for i in range(0, len(zip_paths), batch_size)]
+for batch in batches:
+    p = Pool(batch_size)
+    result_list = p.map(unzip_zip_file, batch)
+    png_paths = []
+    pt_paths = []
+    for result_tuple in result_list:
+        png_paths = png_paths + result_tuple[0]
+        pt_paths = pt_paths + result_tuple[1]
 
     for idx, png_path in enumerate(png_paths):
         pt_path = pt_paths[idx]
@@ -128,11 +150,11 @@ for path in glob.glob(os.path.join(job_output_directory, '*/*.zip'), recursive=T
                                  width=img.size[1])
         dataset['images'].append(image_json)
 
-        tnsr = torch.load(pt_path)
-        for bb in tnsr:
-            anno_json = build_annotation(bb=bb, annotation_id=current_annotation_id, image_id=current_image_id,
-                                         category_id=1)
-            dataset['annotations'].append(anno_json)
+        tensor = torch.load(pt_path)
+        for bb in tensor:
+            annotation_json = build_annotation(bb=bb, annotation_id=current_annotation_id, image_id=current_image_id,
+                                               category_id=1)
+            dataset['annotations'].append(annotation_json)
             current_annotation_id = current_annotation_id + 1
         current_image_id = current_image_id + 1
 
@@ -141,10 +163,9 @@ for path in glob.glob(os.path.join(job_output_directory, '*/*.zip'), recursive=T
     os.makedirs(tmp_extract_dir, exist_ok=True)
     # checkpoint the annotation file.
     json.dump(dataset, open(annotation_save_path, mode='w'))
-    logger.info("Successfully saved annotations after processing zipfile path: {}".format(path))
-    logger.info("After processing zipfile path: {}, current image id: {}, current annotation id: {}".format(path,
-                                                                                                            current_image_id,
-                                                                                                            current_annotation_id))
+    logger.info("Successfully saved annotations after processing zipfile batch paths: {}".format(batch))
+    logger.info("Current image id: {}, current annotation id: {}".format(current_image_id, current_annotation_id))
+
 coco_to_fig_boundaries(figure_boundaries_save_path=figure_boundaries_save_path,
                        coco_annotation_file=annotation_save_path)
 split_train_test(figure_boundaries_path=figure_boundaries_save_path,
