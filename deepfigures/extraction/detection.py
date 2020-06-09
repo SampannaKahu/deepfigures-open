5b75a4c569910b4850b0732d783151a0733f0225
SampannaKahu <sampyash@gmail.com>
2020-06-09 18:25:35 -0400

    Was saving box class. But now saving rects.

--- deepfigures/extraction/detection.py
+++ deepfigures/extraction/detection.py
@@ -122,16 +122,19 @@ def run_detection_on_coco_dataset(dataset_dir: str, images_sub_dir: str, model_s
     annos = json.load(open(os.path.join(dataset_dir, 'figure_boundaries.json')))
     batch_size = 100
     anno_batches = [annos[i:i + batch_size] for i in range(0, len(annos), batch_size)]
-    figure_boxes_by_page = []
-
+    processed_annos = []
     done_counter = 0
     for anno_batch in anno_batches:
         np_image_list = [imageio.imread(os.path.join(dataset_dir, images_sub_dir, anno['image_path'])) for
                          anno in anno_batch]
         _figure_boxes_by_page = detector.get_detections(np_image_list)
         assert len(_figure_boxes_by_page) == len(np_image_list)
-        figure_boxes_by_page = figure_boxes_by_page + _figure_boxes_by_page
-        json.dump(figure_boxes_by_page, open(os.path.join(model_save_dir, output_json_file_name), mode='w'))
+        for _figure_boxes, anno in _figure_boxes_by_page, anno_batch:
+            processed_anno = anno
+            processed_anno['hidden_set_rects'] = [{'x1': box.x1, 'y1': box.y1, 'x2': box.x2, 'y2': box.y2, } for box in
+                                                  _figure_boxes]
+            processed_annos.append(processed_anno)
+        json.dump(processed_annos, open(os.path.join(model_save_dir, output_json_file_name), mode='w'))
         done_counter = done_counter + batch_size
         print("Finished processing: {}".format(done_counter))
 
