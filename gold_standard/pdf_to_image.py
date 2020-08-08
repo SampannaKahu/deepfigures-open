e415f29b085ca10bcebcce87c6f2e612eaa68f7c
SampannaKahu <sampyash@gmail.com>
2020-08-07 20:59:03 -0400

    Modifying the script to convert pdfs to images for training moco model.

--- gold_standard/pdf_to_image.py
+++ gold_standard/pdf_to_image.py
@@ -1,42 +1,49 @@
 import os
 import glob
-import json
 import util
-import shutil
 import logging
+import tempfile
 from deepfigures.utils import settings_utils
 from deepfigures import settings
+from multiprocessing import Pool, cpu_count
+from functools import partial
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(os.path.basename(__file__))
 logger.setLevel(logging.DEBUG)
 
-pdf_renderer = settings_utils.import_setting(settings.DEEPFIGURES_PDF_RENDERER)()
 
-output_dir = '/home/sampanna/mit_etd_data/page_images'
+def convert_pdf_to_images(_pdf_path, _output_dir, _pdf_renderer, _temp_dir, max_pages=500):
+    logger.info("Converting {pdf}.".format(pdf=_pdf_path))
+    with tempfile.TemporaryDirectory(dir=_temp_dir) as td:
+        image_paths = _pdf_renderer.render(
+            pdf_path=_pdf_path,
+            dpi=100,
+            max_pages=max_pages,
+            output_dir=td,
+            use_cache=False
+        )
+        for image_path in image_paths:
+            dest_image_path = os.path.join(_output_dir, os.path.basename(image_path))
+            logger.info("Moving {src} to {dest}".format(src=image_path, dest=dest_image_path))
+            os.rename(image_path, dest_image_path)
+            logger.info("Moved {src} to {dest}".format(src=image_path, dest=dest_image_path))
 
-# input_pattern = '/home/sampanna/mit_etd_data/data/etds/*.pdf'
-# pdf_paths = list(map(os.path.abspath, glob.glob(input_pattern)))
-pdf_paths = ['/home/sampanna/mit_etd_data/data/etds/' + util.handle_to_pdffilename(handle) for handle in
-             json.load(open('data/sampled_etds.json'))]
-logger.info("Total paths obtained: {path_count}".format(path_count=len(pdf_paths)))
 
-for pdf_path in pdf_paths:
-    logger.info(" ")
-    logger.info("----------------------------------------------------------------------------------------")
-    logger.info("Converting {pdf}.".format(pdf=pdf_path))
-    image_paths = pdf_renderer.render(
-        pdf_path=pdf_path,
-        dpi=100,
-        max_pages=500,
-        output_dir=output_dir,
-        use_cache=False
-    )
-    for image_path in image_paths:
-        dest_image_path = os.path.join(output_dir, os.path.basename(image_path))
-        logger.info("Moving {src} to {dest}".format(src=image_path, dest=dest_image_path))
-        os.rename(image_path, dest_image_path)
-        logger.info("Moved {src} to {dest}".format(src=image_path, dest=dest_image_path))
-    if image_paths and os.path.exists(os.path.dirname(os.path.dirname(os.path.dirname(image_paths[0])))):
-        dir_to_delete = os.path.dirname(os.path.dirname(os.path.dirname(image_paths[0])))
-        shutil.rmtree(dir_to_delete)
+def convert_pdf_paths_to_images(input_dir, output_dir, temp, cpu_count=cpu_count(), max_pages=500):
+    pdf_renderer = settings_utils.import_setting(settings.DEEPFIGURES_PDF_RENDERER)()
+    pdf_paths = glob.glob(os.path.join(input_dir, '*.pdf'))
+    logger.info("Total paths obtained: {path_count}".format(path_count=len(pdf_paths)))
+    with Pool(cpu_count) as pool:
+        pool.map(
+            partial(convert_pdf_to_images, _output_dir=output_dir, _pdf_renderer=pdf_renderer, _temp_dir=temp,
+                    max_pages=max_pages),
+            pdf_paths
+        )
+
+
+if __name__ == "__main__":
+    temp = '/work/cascades/sampanna/moco/temp'
+    output_dir = '/work/cascades/sampanna/moco/scanned_etd_dataset'
+    input_dir = '/work/cascades/sampanna/ir_backup/mit_etd_data/data/etds'
+    convert_pdf_paths_to_images(input_dir, output_dir, temp, cpu_count=cpu_count(), max_pages=1000)
