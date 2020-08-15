604974849adce84ff5ed6244cd1f742fa710d297
SampannaKahu <sampyash@gmail.com>
2020-08-15 16:44:10 -0400

    Removed redundant code.

--- tfds_trial/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import logging
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(os.path.basename(__file__))
-logger.setLevel(logging.DEBUG)
-
-
